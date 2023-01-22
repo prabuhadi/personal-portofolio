d6d1af2ad469267dc64c0f3c126430d8c2f4a47c
prabuhadi <prabuhadiyo@gmail.com>
2023-01-23 00:59:26 +0700

    Modify skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,6 +4,7 @@ import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
@@ -63,6 +64,7 @@ export const Skills = () => {
           </Col>
         </Row>
       </Container>
+      {/* <img className="background-image-left" src={colorSharp} /> */}
     </section>
   );
 };
